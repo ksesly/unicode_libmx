0155936175618ccf2abf72e545b4900e9f5856a8
Kseniia <ks.you.04@gmail.com>
2022-11-17 01:58:15 +0200

    printint

--- inc/libmx.h
+++ inc/libmx.h
@@ -10,6 +10,8 @@ void mx_print_unicode(wchar_t c);
 void mx_printchar(char c);
 void mx_printstr(const char *s);
 void mx_print_strarr(char **arr, const char *delim);
+void mx_printint(int n);
+
 
 
 
