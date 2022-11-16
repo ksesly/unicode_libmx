db349f30b56410e673e0b2e842197e294f046eaf
Kseniia <ks.you.04@gmail.com>
2022-11-17 01:56:03 +0200

    print_strarr

--- inc/libmx.h
+++ inc/libmx.h
@@ -3,11 +3,14 @@
 #include <wchar.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int mx_strlen(const char *s); //mod
 void mx_print_unicode(wchar_t c); 
 void mx_printchar(char c);
 void mx_printstr(const char *s);
+void mx_print_strarr(char **arr, const char *delim);
+
 
 
 
