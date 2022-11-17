7c87f17ea4d231f34bbee23751301112ba052961
Kseniia <ks.you.04@gmail.com>
2022-11-17 02:02:29 +0200

    sqrt

--- inc/libmx.h
+++ inc/libmx.h
@@ -12,7 +12,7 @@ void mx_printstr(const char *s);
 void mx_print_strarr(char **arr, const char *delim);
 void mx_printint(int n);
 double mx_pow(double n, unsigned int pow);
-
+int mx_sqrt(int x);
 
 
 
