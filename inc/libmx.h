d186b529245102bc6a531d0937e169c0bc6b9fc3
Kseniia <ks.you.04@gmail.com>
2022-11-17 02:00:06 +0200

    pow

--- inc/libmx.h
+++ inc/libmx.h
@@ -11,6 +11,7 @@ void mx_printchar(char c);
 void mx_printstr(const char *s);
 void mx_print_strarr(char **arr, const char *delim);
 void mx_printint(int n);
+double mx_pow(double n, unsigned int pow);
 
 
 
