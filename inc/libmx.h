afd097d4e4b233dde7f7c33b2867f0824141a1a6
Kseniia <ks.you.04@gmail.com>
2022-11-19 01:12:01 +0200

    getcharindex

--- inc/libmx.h
+++ inc/libmx.h
@@ -34,6 +34,7 @@ char **mx_strsplit(char const *s, char c);
 char *mx_strjoin(char const *s1, char const *s2);
 void mx_foreach(int *arr, int size, void (*f)(int));
 int mx_quicksort(char **arr, int left, int right);
+int mx_get_char_index(const char *str, char c);
 
 
 
