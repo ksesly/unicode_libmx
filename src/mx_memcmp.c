e8b301217844e5382510a2be3e24a2c7dadeb076
Kseniia <ks.you.04@gmail.com>
2022-11-20 02:59:28 +0200

    memcmp

--- src/mx_memcmp.c
+++ src/mx_memcmp.c
@@ -3,7 +3,7 @@
 int mx_memcmp(const void *s1, const void *s2, size_t n) {
 	for (int i = 0; i < (int)n; i++) {
 		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i]) 
-			return (unsigned char *)s1[i] - (unsigned char *)s2[i];
+			return ((unsigned char *)s1)[i] - ((unsigned char *)s2)[i];
 	}
 	return 0; 
 }
